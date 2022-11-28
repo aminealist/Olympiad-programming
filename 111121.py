48608b31118ae25589cb685521080983a337bad6
aminealist <aleshkov.d.o@gmail.com>
2022-11-29 02:16:40 +0700

    Update 111121.py

--- 111121.py
+++ 111121.py
@@ -4,15 +4,19 @@ g = list(map(int, input().replace("\n", "").split(" ")))
 bb=0
 cb=1
 im=0
-for i in range(1, a[0]):
-    if b[i-1]<b[im]:
-        im=i-1
-    if g[cb]*b[im]<g[i]*b[bb]:
-        cb=i
-        bb=im
-if g[cb]-b[bb]<=0:
+if a[0]==1:
     print(a[1])
     print(-1, -1)
 else:
-    print(g[cb]*a[1])
-    print(bb+1, cb+1)
+    for i in range(1, len(g)):
+            if b[i-1]<b[im]:
+                im=i-1
+            if g[cb]*b[im]<g[i]*b[bb]:
+                cb=i
+                bb=im
+    if g[cb]-b[bb]<=0:
+        print(a[1])
+        print(-1, -1)
+    else:
+        print((g[cb])*a[1])
+        print(bb+1, cb+1)
