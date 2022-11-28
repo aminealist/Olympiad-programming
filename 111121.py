58080a957b9440b43ca8003b73d5e3aa3433bf72
aminealist <aleshkov.d.o@gmail.com>
2022-11-28 20:14:27 +0700

    2

---
+++ 111121.py
@@ -0,0 +1,18 @@
+a = list(map(int, input().replace("\n", "").split(" ")))
+b = list(map(int, input().replace("\n", "").split(" ")))
+g = list(map(int, input().replace("\n", "").split(" ")))
+bb=0
+cb=1
+im=0
+for i in range(1, a[0]):
+    if b[i-1]<b[im]:
+        im=i-1
+    if g[cb]*b[im]<g[i]*b[bb]:
+        cb=i
+        bb=im
+if g[cb]-b[bb]<=0:
+    print(a[1])
+    print(-1, -1)
+else:
+    print(g[cb]*a[1])
+    print(bb+1, cb+1)
--- 112735.py
+++ 112735.py
@@ -1,7 +1,13 @@
 a = list(map(int, input().replace("\n", "").split(" ")))
 b = list(map(int, input().replace("\n", "").split(" ")))
 ib=0
-kb=a[1]
+k=a[1]+1
+kb=a[1]+1
 im=0
-for i in range(a[0]):
-    
+for i in range(a[1]+1, a[0]):
+    if b[i-k]>b[im]:
+        im=i-k
+    if b[ib]+b[kb]<b[im]+b[i]:
+        ib=im
+        kb=i
+print(ib+1, kb+1)
